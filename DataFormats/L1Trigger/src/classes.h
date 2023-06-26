f668c2bcd36ce6ee5df1c3e356dfd52b11dde86f
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2023-06-26 21:29:59 +0200

    GTT Vtx z0 scale - update classes

--- DataFormats/L1Trigger/src/classes.h
+++ DataFormats/L1Trigger/src/classes.h
@@ -34,5 +34,6 @@
 #include "DataFormats/L1Trigger/interface/HOTPDigiTwinMux.h"
 #include "DataFormats/L1Trigger/interface/Vertex.h"
 #include "DataFormats/L1Trigger/interface/VertexWord.h"
+#include "DataFormats/L1Trigger/interface/GTVertexWord.h"
 #include "DataFormats/L1Trigger/interface/TkJetWord.h"
 #include "DataFormats/Common/interface/RefToBase.h"
