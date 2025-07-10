03a7f92051476494503d282002df9ef3952ef34c
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2025-07-10 20:12:32 +0200

    Rename correctionlibTPMatchTrack to correctionlibRegression

--- L1Trigger/TrackFindingTracklet/python/Customize_cff.py
+++ L1Trigger/TrackFindingTracklet/python/Customize_cff.py
@@ -142,5 +142,5 @@ def injectSmartPixelsTrackProducer_passthroughHW(process):
 def injectSmartPixelsTrackProducer_trackingParticleTruth(process):
   return injectSmartPixelsTrackProducer(process, smartPixelsEmulatorMode="trackingParticleTruth", skipModuleTypes=None, printProcessInfo=False)
 
-def injectSmartPixelsTrackProducer_correctionlibTPMatchTrack(process):
-  return injectSmartPixelsTrackProducer(process, smartPixelsEmulatorMode="correctionlibTPMatchTrack", skipModuleTypes=None, printProcessInfo=False)
+def injectSmartPixelsTrackProducer_correctionlibRegression(process):
+  return injectSmartPixelsTrackProducer(process, smartPixelsEmulatorMode="correctionlibRegression", skipModuleTypes=None, printProcessInfo=False)
